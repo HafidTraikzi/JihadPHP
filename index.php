673f786f206b1f50de4ca32d2c5c372737df56c2
HafidTraikzi <hafid.traikzi@gmail.com>
2015-01-22 01:57:45 +0100

    Site Apologie de la Paix

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Jihad Scientifique</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="container">
+      <header>
+      </header>
+      <div class="intro">
+        <?php echo "Il n'existe qu'un seul Dieu, Thot, tous les mécréants qui prétente le contraire seront menacés d'une fatwa. La seule véritable religion est la science. Elle est naît pour unir les hommes et les femmes dans la vérité."; ?>
+      </div>
+      <div class="title">
+        <?php echo "Sourates :" ?>
+      </div>
+      <div class="pic">
+        <img src="http://img15.hostingpics.net/pics/147606AlbertEinstein100x125.jpg"></img>   
+      </div>
+      <div class="citation">
+      <?php echo "Albert Einstein dit : \"Rare est le nombre de ceux qui regardent avec leurs propres yeux et qui éprouvent avec leur propre sensibilité.\"" ?>
+      </div>
+      <div class="pic">
+        <img src="http://img15.hostingpics.net/pics/922086IsaacNewton100x125.jpg"></img>
+      </div>
+      <div class="citation">
+        <?php echo "Isaac Newton dit : \"Les hommes construisent trop de murs et pas assez de ponts.\"<br>" ?>
+      </div>
+      <div class="slide1">
+      </div>
+      <div class="title">
+        <?php echo "Devenir un moudjahid" ?>
+      </div>
+      <div class="form">
+        <form action="action.php" method="post">
+          <p>Votre nom : <input type="text" name="name"></p>
+          <p>Votre âge : <input type="text" name="age"></p>
+          <p>Vos motivations : <input type="text" size="60" name="motivation"></p>
+          <p><input type="submit" value="OK"></p>
+        </form>
+      </div>
+      <div class="slide2">
+      </div>
+      <footer>
+        <?php echo "Apologie de la Paix, avec un peu de réflexion" ?>
+      </footer>
+    </div>
+  </body>
+</html>
