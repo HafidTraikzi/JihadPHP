b52806d7384faf33aafd1a6061ab940d4a0c37ff
HafidTraikzi <hafid.traikzi@gmail.com>
2015-01-23 16:45:08 +0100

    Modification variable  et suppression d'une margin dans le css

--- form.php
+++ form.php
@@ -2,16 +2,16 @@
 
 if (isset($_POST) && isset($_POST["name"]) && isset($_POST["age"]) && isset($_POST["email"]) && isset($_POST["motivation"])) {
   if (!empty($_POST["name"]) && !empty($_POST["age"]) && !empty($_POST["email"]) && !empty($_POST["motivation"])) {
-  $destinataire = "hafid.traikzi@gmail.com";
-  $sujet = "Demande de contact";
-  $message = "Nom : ".$_POST["name"]."\r\n";
-  $message = "Age : ".$_POST["age"]."\r\n";
-  $message = "Adresse email : ".$_POST["email"]."\r\n";
-  $message = "Message : ".$_POST["motivation"]."\r\n";
-	$entete = "From: ".$_POST["email"]."\r\n".
+  $to = "hafid.traikzi@gmail.com";
+  $subject = "Demande de contact";
+  $message = "Nom : ".$_POST["name"]."\r\n
+              Age : ".$_POST["age"]."\r\n
+              Adresse email : ".$_POST["email"]."\r\n
+              Message : ".$_POST["motivation"]."\r\n";
+	$header = "From: ".$_POST["email"]."\r\n".
            	"Reply-To: ".$_POST["email"]."\r\n".
 	    	    'X-Mailer: PHP/'.phpversion();
-  if (mail($destinataire, $sujet, $message, $entete)) {
+  if (mail($to, $subject, $message, $header)) {
     echo "Message envoyé";
   } else {
  		echo "Une erreur est survenue lors de l'envoi du formulaire par email";
